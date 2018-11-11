253592f8f3a23d6d6e62c4ccb4e2e8af9354d552
Yummykibbles034 <43100416+Yummykibbles034@users.noreply.github.com>
2018-11-10 23:41:27 -0500

    Add files via upload

--- CompetitionTeleop.java
+++ CompetitionTeleop.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Competition;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,12 +8,14 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp
 public class CompetitionTeleop extends OpMode{
 
-    private DcMotor Backleft, Backright;
+    private DcMotor Backleft, Backright, LinearSlide, IntakeArm;
 
 
     public void init() {
         Backleft = hardwareMap.dcMotor.get("Backleft");
         Backright = hardwareMap.dcMotor.get("Backright");
+        LinearSlide = hardwareMap.dcMotor.get ("Linear");
+        IntakeArm = hardwareMap.dcMotor.get ("Arm");
 
     }
 
@@ -23,6 +25,32 @@ public class CompetitionTeleop extends OpMode{
         Backleft.setPower(gamepad1.left_stick_y - gamepad1.right_stick_x);
         Backright.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x);
 
+        if (gamepad1.right_bumper){
+
+            LinearSlide.setPower(1.0);
+
+        }
+        else if (gamepad1.left_bumper) {
+
+            LinearSlide.setPower(-1.0);
+        }
+        else {
+
+            LinearSlide.setPower(0);
+        }
+
+        if (gamepad1.a){
+
+            IntakeArm.setPower(0.5);
+        }
+        else if (gamepad1.b){
+
+            IntakeArm.setPower(-0.5);
+        }
+        else {
+
+            IntakeArm.setPower(0);
+        }
     }
 
 }
