067adc78c8a6d1be5d129d19e6bd478c28f598d4
Yummykibbles034 <43100416+Yummykibbles034@users.noreply.github.com>
2019-01-15 23:23:18 -0500

    Add files via upload

---
+++ K9RobotTeleop.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class K9RobotTeleop extends OpMode {
+    private DcMotor Backleft, Backright;
+    private CRServo Arm, Claw;
+}
